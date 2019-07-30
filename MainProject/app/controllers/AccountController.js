7803f321a59f6ee61b13ff5287ea71760c838bfc
rajibsahani29 <rajibsahani29@gmail.com>
2019-07-30 12:07:25 +0530

    newly added for account section and common header

---
+++ MainProject/app/controllers/AccountController.js
@@ -0,0 +1,29 @@
+﻿(function () {
+
+    "use strict";
+
+    angular
+        .module("iAspireApp")
+        .controller("AccountController", ["DataService", "$scope", "$rootScope", "$location", "SMAAlertFactory", "ProjectConstants", accountController]);
+
+    function accountController(DataService, $scope,$rootScope, $location, SMAAlertFactory, ProjectConstants) {
+       
+        var accountC = this;
+        accountC.UserName = null;
+        accountC.UserID = null;
+        accountC.UserEmail = null;
+        accountC.MerchantName = null;
+        accountC.MerchantID = null;
+        accountC.UserRoles = null;
+        //return accountC;
+        var userdata = JSON.parse(localStorage.getItem("MainUserData"));
+        accountC.UserName = userdata.FirstName +" "+ userdata.LastName;
+        accountC.UserID = userdata.UserID;
+        accountC.UserEmail = userdata.Email;
+        accountC.UserRoles = userdata.UserRoles;
+        accountC.MerchantName = userdata.MerchantName;
+        accountC.MerchantID = userdata.MerchantID;
+        
+    }
+
+})();
---
+++ MainProject/app/controllers/HeaderController.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("iAspireApp")
+        .controller("HeaderController", ["$scope","$rootScope", "DataService", "SMAAlertFactory", "ProjectConstants", headerController]);
+
+    function headerController($scope, $rootScope, DataService, SMAAlertFactory, ProjectConstants) {
+        var headC = this;
+        headC.AnimateMenu = AnimateMenu;
+        headC.Logout = logout;    
+
+        $scope.userName = localStorage.getItem("iAsp.LoginName");
+        if ($scope.userName == "") $scope.userName = "Account";
+          
+        return headC;
+        function AnimateMenu(elem) {
+            var btnclasschk = $('#btn').hasClass("dl-active")
+            var menuclasschk = $('#ulmenu').hasClass("dl-menuopen");
+            if (!btnclasschk && !menuclasschk) {
+                $('#btn').addClass("dl-active");
+                $('#ulmenu').addClass("dl-menuopen");
+            } else {
+                $('#btn').removeClass("dl-active");
+                $('#ulmenu').removeClass("dl-menuopen");
+            }
+        }
+        function logout() {
+            function confirmCallback(val) {
+                if (val === true) {
+                    DataService.userLogout()
+                        .success(function (data, status, headers, config) {
+                            $scope.AppC.ActiveUser.UserID = null;
+                            $rootScope.ID = null;
+                            location.hash = '#login';
+                        })
+                        .error(function (data, status, headers, config) {
+                            // we don't really care if it failed... the user clicked logout
+                            $scope.AppC.ActiveUser.UserID = null;
+                        });
+                }
+            }
+            SMAAlertFactory.CreateConfirmAlert("Are you sure you want to log out?", null, null, null, confirmCallback);
+        }
+    }
+
+})();
